83ecfaeaabbbc07852dcad9d97d623e91490dbd8
Madhvi <madhvisan5277@gmail.com>
2024-12-24 13:26:41 +0530

    components call in each other

--- Demo/src/LEC2412/component1.jsx
+++ Demo/src/LEC2412/component1.jsx
@@ -1,10 +1,32 @@
 import * as React from "react";
 
+class Component2 extends React.Component{
+    render() {
+        return (
+            <h2>Component 2</h2>
+        );
+    }
+}
+
+class Component3 extends React.Component{
+    render() {
+        return (
+            <h3>Component 3</h3>
+        );
+    }
+}
+
 // each class must contain render function
+// can call componenets in each other too
 class Component1 extends React.Component{
     render() {
         return(
-            <h1>Componenet 1</h1>
+            <div>
+                <h1>Component 1</h1>
+                <h2>Hello</h2>
+                <Component2 />
+                <Component3 />
+            </div>
         );
     }
 }
