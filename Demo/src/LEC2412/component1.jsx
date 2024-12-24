2bf3a08ad8f66aca95d7a172a2c2fa423303fa8c
Madhvi <madhvisan5277@gmail.com>
2024-12-24 13:21:12 +0530

    component creation

---
+++ Demo/src/LEC2412/component1.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+// each class must contain render function
+class Component1 extends React.Component{
+    render() {
+        return(
+            <h1>Componenet 1</h1>
+        );
+    }
+}
+export default Component1;
